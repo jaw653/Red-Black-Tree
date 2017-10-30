dd1aeee93f01042bf0abe8ecb4d60ca4bec47d78
jaw653 <jawax10@gmail.com>
2017-10-29 21:11:44 -0500

    struct made w/ constructor

--- rbt.c
+++ rbt.c
@@ -4,5 +4,31 @@
  * The University of Alabama
  *
  * This file serves as the implementation for the red-black tree interface.
- *
  */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "rbt.h"
+#include "bst.h"
+struct rbt {
+    struct bst *tree;
+    void (*display)(FILE *, void *);
+    int (*comparator)(void *, void *);
+};
+
+RBT *newRBT(
+  void (*d)(FILE *, void *),
+  int (*c)(void *, void *)
+)
+{
+    RBT *t = malloc(sizeof(struct rbt));
+    t->tree = newBST(d, c, NULL);
+    t->display = d;
+    t->comparator = c;
+
+    return t;
+}
+
+void insertBST(RBT *t, void *value) {
+  //use pseudo code here
+}
