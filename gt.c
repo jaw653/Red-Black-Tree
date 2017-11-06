42c85095df21cf1f189c45838c91b23b2ab1e273
jaw653 <jawax10@gmail.com>
2017-11-05 23:59:52 -0600

    added all functions to gt.c (untested)

--- gt.c
+++ gt.c
@@ -24,3 +24,54 @@ GT *newGT(void (*d)(FILE *, void *), int (*c)(void *, void *)) {
 
   return t;
 }
+
+void insertGT(GT *t, void *value) {
+  BSTNODE *x = insertBST(t->tree, value);
+
+  if (x == NULL) {
+    BSTNODE *newNode = findBST(t->tree, value);
+    incrementGTNODEfrequency(newNode);
+  }
+  else {
+    incrementGTNODEfrequency(x);
+  }
+}
+
+int findGT(GT *t, void *value) {
+  GTNODE *p = getGTNODE(findBST(t->tree, value));
+
+  /* Value is not in the tree */
+  if (p == NULL) {
+    return 0;
+  }
+  else {
+    return getGTNODEfrequency(p);
+  }
+}
+
+void deleteGT(GT *t, void *value) {
+  BSTNODE *valueToDelete = findBST(t->tree, value);
+
+  if (valueToDelete != NULL) {
+    int newFreq = decrementGTNODEfrequency(getGTNODE(valueToDelete));
+    if (newFreq == 0) {
+      deleteBST(t->tree, value);
+    }
+  }
+}
+
+int sizeGT(GT *t) {
+  return sizeBST(t->tree);
+}
+
+int wordsGT(GT *t) {
+  //return the number of words (?)
+}
+
+void statisticsGT(FILE *fp, GT *t) {
+  //need to figure this out
+}
+
+void displayGT(FILE *fp, GT *t) {
+  displaBST(fp, t->tree);
+}
